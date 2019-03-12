e9a3e4b8f25fb118fc9bc3ef87612476dc58ab5f
Julia Hardy <julia.joanna.hardy@gmail.com>
2019-03-12 19:10:12 +0100

    css transition

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let tea_products = document.querySelector(".tea-products");
 let tea_images = tea_products.querySelectorAll("img");
-let modal = tea_products.querySelector(".modal")
-let modal_img = tea_products.querySelectorAll("#tea-img");
-let text = tea_products.querySelectorAll("#caption");
 
-console.log(tea_products)
 
 function showDetails(){
-    modal.style.display = "block";
+    let selected_div = this.parentElement;
+    let description = selected_div.querySelectorAll(".product-description");
 
+    description.forEach(desc => desc.style.display = "block");
+    selected_div.style.backgroundColor = "white";
+    selected_div.classList.toggle('description-img');
+    // this.style.width = "300px";
+    // this.style.height ="200px";
 }
 
 tea_images.forEach(image => image.addEventListener("click", showDetails));
